fcfc163700979ef08f3cb85ac7ccb6d8c94fa82e
Yancheng Cai <yc613@dc.cl.cam.ac.uk>
2023-12-11 01:13:06 +0000

    2023-12-11

--- VRR_Subjective_Experiment/draw_temporal_flicker_meter.py
+++ VRR_Subjective_Experiment/draw_temporal_flicker_meter.py
@@ -1,8 +1,9 @@
 import os
 import matplotlib.pyplot as plt
 import json
-
-base_path = 'E:\Datasets\Subjective_Exp_1\Subjective_Exp_1'
+import numpy as np
+# base_path = 'E:\Datasets\Subjective_Exp_1\Subjective_Exp_1'
+base_path = 'B:\Subjective_Exp_1'
 
 def draw_temporal_flicker_meter(Luminance_value, Size_value, VRR_Frequency, repeat_index):
     file_name = os.path.join(base_path, f'Luminance_{Luminance_value}_Size_{Size_value}_VRR_Frequency_{VRR_Frequency}',
@@ -37,15 +38,50 @@ def draw_temporal_flicker_meter_repeat_set(Luminance_value, Size_value, VRR_Freq
         plt.ylabel('Luminance (nits)')
     plt.show()
 
+def draw_temporal_flicker_meter_downsample(Luminance_value, Size_value, VRR_Frequency, repeat_index, downsample_rate):
+    file_name = os.path.join(base_path, f'Luminance_{Luminance_value}_Size_{Size_value}_VRR_Frequency_{VRR_Frequency}',
+                             f'{repeat_index}.json')
+    with open(file_name, 'r') as fp:
+        signal_data = json.load(fp)
+    x_time_array = signal_data['x_time']
+    y_luminance_array = signal_data['y_luminance_scale']
+    # x_time_downsampled = x_time_array[::downsample_rate]
+    # y_luminance_downsampled = y_luminance_array[::downsample_rate]
+    x_time_downsampled = []
+    y_luminance_downsampled = []
+
+    for i in range(0, len(x_time_array), downsample_rate):
+        chunk = slice(i, i + downsample_rate)
+        x_chunk = x_time_array[chunk]
+        y_chunk = y_luminance_array[chunk]
+
+        # Take the average of the chunk
+        x_time_downsampled.append(np.mean(x_chunk))
+        y_luminance_downsampled.append(np.mean(y_chunk))
+    plt.figure()
+    plt.plot(x_time_downsampled, y_luminance_downsampled)
+    plt.xlim(0, 0.25)
+    # plt.ylim(0, max(y_luminance_downsampled) * 1.1)
+    plt.ylim(0, 4)
+    plt.xlabel('Time in seconds')
+    plt.ylabel('Luminance (nits)')
+    plt.title(f'Down sampling rate: {downsample_rate}')
+    plt.show()
+
 if __name__ == '__main__':
-    Luminance_value = 100
+    Luminance_value = 2
     Size_value = 4
     VRR_Frequency = 10
     repeat_index = 1
-    draw_temporal_flicker_meter(Luminance_value=Luminance_value,
-                                Size_value=Size_value,
-                                VRR_Frequency=VRR_Frequency,
-                                repeat_index=repeat_index)
+    # draw_temporal_flicker_meter(Luminance_value=Luminance_value,
+    #                             Size_value=Size_value,
+    #                             VRR_Frequency=VRR_Frequency,
+    #                             repeat_index=repeat_index)
     # draw_temporal_flicker_meter_repeat_set(Luminance_value=Luminance_value,
     #                                        Size_value=Size_value,
     #                                        VRR_Frequency=VRR_Frequency)
+    draw_temporal_flicker_meter_downsample(Luminance_value=Luminance_value,
+                                Size_value=Size_value,
+                                VRR_Frequency=VRR_Frequency,
+                                repeat_index=repeat_index,
+                                downsample_rate=10)
--- plot_from_saved_json.py
+++ plot_from_saved_json.py
@@ -2,7 +2,7 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-save_json_path = 'plt_save_fig/MTime_2023-10-27/[60,120,60]-[2,2,2,2,2]-[1.0,1.0,1.0]-Noblack.json'
+save_json_path = 'B:\Subjective_Exp_1\Luminance_1_Size_4_Fix_60Hz/0.json'
 
 with open(save_json_path, 'r') as fp:
     json_data = json.load(fp)
@@ -12,7 +12,7 @@ y_luminance_array = np.array(json_data['y_luminance'])
 
 plt.figure()
 plt.plot(x_time_array,y_luminance_array)
-plt.xlim([0,10])
-plt.ylim([0,1])
+plt.xlim([0,0.25])
+plt.ylim([0,max(y_luminance_array)])
 plt.show()
 
