d08be6a9782782ddb250b54434bf0767c1a5dadf
Yancheng Cai <yc613@dc.cl.cam.ac.uk>
2023-12-11 00:24:04 +0000

    2023-12-11

--- temporal_flick_meter_measure_main.py
+++ temporal_flick_meter_measure_main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 sensor = temporal_light_sensor.TemporalLightSensor(serial.Serial("COM4", 500000))
-num_measurements = 50000
+num_measurements = 50000 #51999是极限，但是一般不建议超过50000，注意会卡住
 sampling_frequency = 50000
 sensor.take_measurement(num_measurements=num_measurements, sampling_frequency=sampling_frequency)
 
@@ -16,3 +16,10 @@ plt.xlim((0, 0.25))
 plt.ylim((0, 1))
 plt.show()
 # plt.savefig(r'E:\Py_codes\Py_Temporal_Light_Sensor_GIGA\plt_save_fig/try.png')
+
+# 有一种办法可以让读取数据和显示数据共存，例如，要求temporal flicker meter 每隔2s, 收集2s数据，之后盖上时间戳，然后和显示光那边的时间戳对应就好了（但是最好还能有一个Konica可以实时地读取数据
+# 建议使用三台机器
+# 1： My Laptop: 显示光照
+# 2： My PC: 记录Temporal Flicker Meter
+# 3： Rainbow Lab PC: 记录Knoica光强
+# 就在我办公室记录就行咧
