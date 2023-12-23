edc564f9298dcd1875c45efa99f8aa63f4d02609
unknown <19307140030@fudan.edu.cn>
2023-12-23 14:40:43 +0000

    2023-12-23

--- dL_L/LG_G1_Calibration.py
+++ dL_L/LG_G1_Calibration.py
@@ -149,6 +149,8 @@ def check_dl_L_all(Size, Pixel_value_range, sample_numbers, scale, Refresh_rate,
                 log_data[str(frame_rate)] = response
         print('Response', response)
         json_log_data[f'S_{size_value}_C_{color_value}_R_{repeat_index}'] = log_data
+        with open(os.path.join(save_dir_path, 'result_in_progress.json'), 'w') as fp:
+            json.dump(json_log_data, fp)
     with open(os.path.join(save_dir_path, 'result.json'), 'w') as fp:
         json.dump(json_log_data, fp)
 
@@ -158,9 +160,9 @@ if __name__ == "__main__":
     sample_numbers = 30
     scale = 'Log10' #Linear/Log10
     Refresh_rate = [30, 120]
-    repeat_times = 4
+    repeat_times = 10
     # 别忘了denser at darker
-    save_dir_path = f"../dL_L/LG_G1_KONICA_9"
+    save_dir_path = f"../dL_L/LG_G1_KONICA_10"
     os.makedirs(save_dir_path, exist_ok=True)
     config_json = {'Size': Size, 'Pixel_value_range': Pixel_value_range,
                    'sample_numbers': sample_numbers, 'scale': scale,
